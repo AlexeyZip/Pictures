6214966ec61215b549f3aabc2b34de469a962e2f
anuitexfrontenddev <rentbetta.anuitexdev2@gmail.com>
2020-10-07 08:59:37 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import checkTextInputs from './modules/checkTextInputs';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import modals from './modules/modals';
+import showMoreStyles from './modules/showMoreStyles';
+import sliders from './modules/sliders';
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
+
+})
+
+
+
